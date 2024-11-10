1b7d4ed0bbe13f471128ce421ab67cd35616f8d7
Aymen Letaief <aymen.letaief@kaiahealth.com>
2024-11-10 10:43:40 +0100

    added initial FoodItemsApi module for loading FoodItems

---
+++ FoodItemsApi/FoodItem.swift
@@ -0,0 +1,13 @@
+//
+//  FoodItem.swift
+//  FoodApi
+//
+//  Created by Aymen Letaief on 2024-11-10.
+//
+
+struct FoodItem {
+    let name: String
+    let caloryCount: Int
+    let proteinCount: Int
+    let fatCount: Int
+}
---
+++ FoodItemsApi/FoodItemsLoader.swift
@@ -0,0 +1,12 @@
+//
+//  FoodItemsLoader.swift
+//  FoodApi
+//
+//  Created by Aymen Letaief on 2024-11-10.
+//
+
+protocol FoodItemsLoader {
+}
+
+class RemoteFoodItemsLoader: FoodItemsLoader {
+}
