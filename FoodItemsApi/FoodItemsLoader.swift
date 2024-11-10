ae528844a032662c35816cf850add921a6307988
Aymen Letaief <aymen.letaief@kaiahealth.com>
2024-11-10 12:51:44 +0100

    added loadfooditems and tested integration of the httpclient with RemoteFoodItemsLoader

--- FoodItemsApi/FoodItemsLoader.swift
+++ FoodItemsApi/FoodItemsLoader.swift
@@ -5,8 +5,28 @@
 //  Created by Aymen Letaief on 2024-11-10.
 //
 
+import Foundation
+
 protocol FoodItemsLoader {
+    func loadFoodItems() async -> [FoodItem]
+}
+
+protocol HTTPClient {
+    func loadURL(url: URL) async -> Data
 }
 
 class RemoteFoodItemsLoader: FoodItemsLoader {
+    let client: HTTPClient
+    let url: URL
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    func loadFoodItems() async -> [FoodItem] {
+        await client.loadURL(url: url)
+        return []
+    }
+    
 }
--- FoodItemsApiTests/FoodItemsApiTests.swift
+++ FoodItemsApiTests/FoodItemsApiTests.swift
@@ -8,15 +8,33 @@
 import XCTest
 @testable import FoodItemsApi
 
-final class FoodItemsApiTests: XCTestCase {
+final class RemoteFoodItemsApiTests: XCTestCase {
     
-    func testSUT_isNotNil() {
-        let sut = makeSUT()
-        XCTAssertNotNil(sut)
+    func testSUT_LoaderisNotNil() {
+        let (loader, _) = makeSUT()
+        XCTAssertNotNil(loader)
+    }
+    
+    @MainActor
+    func testSUT_loadFoodItemsTriggersLoadingURL() async {
+        let url = URL(string: "www.sampleUrl.com")!
+        let (loader, client) = makeSUT(url: url)
+        let _ = await loader.loadFoodItems()
+        XCTAssertEqual(client.loadedURLs, [url])
     }
 
-    private func makeSUT() -> FoodItemsLoader {
-        RemoteFoodItemsLoader()
+    private func makeSUT(url: URL = URL(string: "www.aUrl.com")!) -> (RemoteFoodItemsLoader, HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        return (RemoteFoodItemsLoader(url: url, client: client), client)
     }
 
+}
+
+private final class HTTPClientSpy: HTTPClient {
+    var loadedURLs: [URL] = []
+    
+    func loadURL(url: URL) async -> Data {
+        loadedURLs.append(url)
+        return Data()
+    }
 }
