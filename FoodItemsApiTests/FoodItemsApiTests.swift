9739297434b380542b357816464ca1cdf028213d
Aymen Letaief <aymen.letaief@kaiahealth.com>
2024-11-10 16:04:43 +0100

    improved the readability of the tests and removed code duplication

--- FoodItemsApiTests/FoodItemsApiTests.swift
+++ FoodItemsApiTests/FoodItemsApiTests.swift
@@ -27,37 +27,42 @@ final class RemoteFoodItemsApiTests: XCTestCase {
     func testSUT_LoaderFailsWhenClientFails() async throws {
         let url = URL(string: "https://www.sampleUrl.com")!
         let (loader, client) = makeSUT(url: url)
-        let triggeredError = NSError(domain: "triggeredError", code: 1)
-        client.triggerError = triggeredError
-        
-        do {
+        let error = NSError(domain: "triggeredError", code: 1)
+        client.triggerError = error
+        await expect(loader: loader, toThrow: error, when: {
             _ = try await loader.loadFoodItems()
-            XCTFail("Expected error, but loader completed successfully")
-        } catch {
-            XCTAssertEqual(error as NSError, triggeredError, "Expected the triggered error to be thrown.")
-        }
-        
-        XCTAssertEqual(client.loadedURLs, [url], "The loader should have attempted loading from the specified URL.")
+        })
     }
     
     func testSUT_loaderThrowsErrorOnInvalidResponse() async throws {
         let url = URL(string: "https://www.sampleUrl.com")!
-        
         let (loader, client) = makeSUT(url: url)
-        client.returnedResponse = HTTPURLResponse(
-            url: url,
-            statusCode: 404,
-            httpVersion: nil,
-            headerFields: nil
+        let invalidResponses = [400,500,199,304,208].map(
+            {
+                HTTPURLResponse(
+                    url: url,
+                    statusCode: $0,
+                    httpVersion: nil,
+                    headerFields: nil
+                )
+            }
         )
+        
+        for invalidResponse in invalidResponses {
+            client.returnedResponse = invalidResponse
+            await expect(loader: loader,toThrow: RemoteFoodItemError.invalidResponseCode, when: {
+                _ = try await loader.loadFoodItems()
+            })
+        }
+    }
+    
+    private func expect<T: Error & Equatable>(loader: RemoteFoodItemsLoader, toThrow expectedError: T, when: () async throws -> Void) async {
         do {
-            _ = try await loader.loadFoodItems()
+            try await when()
             XCTFail("Expected error, but loader completed successfully")
         } catch {
-            XCTAssertEqual(error as? RemoteFoodItemError, .invalidResponseCode, "Expected the triggered error to be thrown.")
+            XCTAssertEqual(error as? T, expectedError, "Expected the triggered error to be thrown.")
         }
-        
-        XCTAssertEqual(client.loadedURLs, [url], "The loader should have attempted loading from the specified URL.")
     }
     
     private func makeSUT(url: URL = URL(string: "www.aUrl.com")!) -> (RemoteFoodItemsLoader, HTTPClientSpy) {
