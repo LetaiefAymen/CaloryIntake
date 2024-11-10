146f2e1cf6fc365d2695305f30b626c6b777cde1
Aymen Letaief <aymen.letaief@kaiahealth.com>
2024-11-10 09:51:17 +0100

    Added initial project

---
+++ CaloryIntake/CaloryIntakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloryIntakeApp.swift
+//  CaloryIntake
+//
+//  Created by Aymen Letaief on 2024-11-10.
+//
+
+import SwiftUI
+
+@main
+struct CaloryIntakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaloryIntake/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CaloryIntake
+//
+//  Created by Aymen Letaief on 2024-11-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
